6596794137dc7e67d5399bc820765b7e2df9fad2
Ruiqi-f-Huang <ruiqi.f.huang@gmail.com>
2026-02-21 15:31:56 +0800

    LC-house-robber [Runtime: 0 ms; Memory: 19.4 MB]

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -11,21 +11,34 @@ class Solution:
         # ans = dfs(n-1)
         # return ans
 
-        ## Step 2: memo
-        cache = [-1] * n
-        def dfs(i):
-            if i < 0:
-                return 0
-            if cache[i] != -1:
-                return cache[i]
+        ## Step 2: memo - space limiting
+        # cache = [-1] * n
+        # def dfs(i):
+        #     if i < 0:
+        #         return 0
+        #     if cache[i] != -1:
+        #         return cache[i]
             
-            ans = max(dfs(i-1), dfs(i-2)+nums[i])
-            cache[i] = ans
-            return ans
+        #     ans = max(dfs(i-1), dfs(i-2)+nums[i])
+        #     cache[i] = ans
+        #     return ans
        
-        return dfs(n-1)
+        # return dfs(n-1)
 
+        ## Step 3: recursion - space limiting
+        # f = [0] * (n+2)
+        # for i, x in enumerate(nums):
+        #     f[i+2] = max(f[i+1], f[i]+x)
 
+        # return f[n+1]
+
+        ## Step 4: recursion with space optimization
+        f0 = f1 = 0 # f1: last state, f0: last last state
+        for i, x in enumerate(nums):
+            newF = max(f1, f0 + x)
+            f0 = f1
+            f1 = newF
+        return f1
 
 
         
