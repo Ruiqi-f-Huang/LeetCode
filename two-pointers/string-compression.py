f11d068a18a21e1284340ed723d08cad851bd847
Ruiqi-f-Huang <ruiqi.f.huang@gmail.com>
2026-03-10 13:03:14 +0800

    LC-string-compression [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/string-compression.py
@@ -0,0 +1,24 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        n = len(chars)
+        l = r = 0
+        write = 0
+
+        while r < n:
+            while r < n and chars[r] == chars[l]:
+                r += 1
+            
+            char = chars[l]
+            num = r - l
+
+            chars[write] = char
+            write += 1
+            if num != 1:
+                for c in str(num):
+                    chars[write] = c
+                    write += 1
+            
+            l = r
+            r += 1
+        
+        return write
