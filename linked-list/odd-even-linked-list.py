846249888e1e444cd456aa7db361d5211cece9b3
Ruiqi-f-Huang <ruiqi.f.huang@gmail.com>
2026-03-11 15:45:05 +0800

    LC-odd-even-linked-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/odd-even-linked-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd = head
+        even = even_start = head.next
+        while odd and even:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = even_start
+        return head
