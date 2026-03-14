26bb509250d46d1967bc3214b0be4eb9c9835dc0
Ruiqi-f-Huang <ruiqi.f.huang@gmail.com>
2026-03-14 19:53:25 +0800

    LC-permutations [Runtime: 4 ms; Memory: 19.4 MB]

---
+++ array/permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans = []
+        path = []
+
+        def dfs(i, s):
+            if i == n:
+                ans.append(path.copy())
+                return
+            
+            for x in s:
+                path.append(x)
+                dfs(i+1, s-{x})
+                path.pop()
+
+        dfs(0, set(nums))
+        return ans
