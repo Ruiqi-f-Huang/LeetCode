a35dd05aac99e09cd54b448a7c20209cb45394e6
Ruiqi-f-Huang <ruiqi.f.huang@gmail.com>
2026-03-14 16:22:32 +0800

    LC-longest-consecutive-sequence [Runtime: 43 ms; Memory: 36.6 MB]

--- hash-table/longest-consecutive-sequence.py
+++ hash-table/longest-consecutive-sequence.py
@@ -1,14 +1,29 @@
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         if not nums:
+#             return 0
+#         st = set(nums)
+#         ans = 1
+#         for x in st:
+#             if x - 1 not in st:
+#                 continue
+#             i = 1
+#             while x + i in st:
+#                 i += 1
+#             ans = max(ans, i+1)
+#         return ans
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        st = set(nums)
-        ans = 1
-        for x in st:
-            if x - 1 not in st:
+        st = set(nums)  # 把 nums 转成哈希集合
+        ans = 0
+        for x in st:  # 遍历哈希集合
+            if x - 1 in st:  # 如果 x 不是序列的起点，直接跳过
                 continue
-            i = 1
-            while x + i in st:
-                i += 1
-            ans = max(ans, i+1)
+            # x 是序列的起点
+            y = x + 1
+            while y in st:  # 不断查找下一个数是否在哈希集合中
+                y += 1
+            # 循环结束后，y-1 是最后一个在哈希集合中的数
+            ans = max(ans, y - x)  # 从 x 到 y-1 一共 y-x 个数
         return ans
